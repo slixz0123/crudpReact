da0675e282b7503a0aed7f9e4c83cfd67e62121e
Slixz0 <90061842+slixz0123@users.noreply.github.com>
2023-02-25 21:01:31 -0500

    up

---
+++ rest-api-springboot-mvc-master/src/main/java/com/hg/crud/CrudApplication.java
@@ -0,0 +1,18 @@
+package com.hg.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+
+public class CrudApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
